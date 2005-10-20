1070c08e74f8faefae55aca51c5f1d1cc38a91c2
radivot@hal.EPBI.cwru.edu <radivot@hal.EPBI.cwru.edu@bc3139a8-67e5-0310-9ffc-ced21a209358>
2005-10-20 12:11:18 +0000

    git-svn-id: https://hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/SBMLR@14545 bc3139a8-67e5-0310-9ffc-ced21a209358

--- inst/BMCcancerFolates/Fig7cheungData.r
+++ inst/BMCcancerFolates/Fig7cheungData.r
@@ -1,11 +1,11 @@
 # This is Morrison's folate model driven by Vivian Cheung et al's Radiation time course response data (GDS479).
-setwd(file.path(.path.package("SBMLR"), "BMCcancerFolates")) #default dump site 
-#setwd("C:/cwru/active/Morrison")  # set this to where figs should be dumped, with comment removed
 library(Biobase)
 library(odesolve)
 library(annotate)
 library(hgu95av2)
 library(SBMLR)  
+setwd(file.path(.path.package("SBMLR"), "BMCcancerFolates")) #default dump site 
+#setwd("C:/cwru/active/Morrison")  # set this to where figs should be dumped, with comment removed
 
 
 morr=readSBMLR(file.path(.path.package("SBMLR"), "models/morrison.r"))  
