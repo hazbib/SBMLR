711699490c98046157c7080b6ce74b7b8fb2fb69
hazbib <hamzeh.zbib@pnnl.gov>
2016-11-09 15:29:51 -0800

    time

--- R/sim.r
+++ R/sim.r
@@ -68,7 +68,7 @@
   
   if (is.null(X0)) 
     X0=mi$y0
-    times=times
+    times=seq(0,100,1)
     out=ode(y=X0,times=times,fderiv,  parms=c(mod=mod), ...)
 #       out=ode(y=mi$y0,times=times,fderiv,  parms=c(mod=mod),  rtol=1e-4, atol= my.atol) 
 #   out=lsoda(y=X0,times=times,fderiv,  parms=c(mod=mod),  rtol=1e-4, atol= my.atol) else
