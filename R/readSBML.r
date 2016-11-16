9c28722c9d62f9413a8770c70714dc607d1ed7d2
hazbib <hamzeh.zbib@pnnl.gov>
2016-11-16 08:40:58 -0800

    fix functions

--- R/readSBML.r
+++ R/readSBML.r
@@ -531,15 +531,15 @@
     if (nFunctions>0){
       for (i in 1:nFunctions)
       {
-#        model$functions[[i]]$mathmlLaw=functions[[i]][["math"]][[1]]
-        mathml<-functions[[i]][["math"]][["lambda"]][["bvar"]][[1]]
-        model$functions[[i]]$mathmlLaw=mathml
+        model$functions[[i]]$mathmlLaw=functions[[i]][["math"]][["lambda"]][["bvar"]][[1]]
+#        mathml<-functions[[i]][["math"]][["lambda"]][["bvar"]][[1]]
+        mathml<-model$functions[[i]]$mathmlLaw
         e<-mathml2R(mathml)
         model$functions[[i]]$exprLaw<-e[[1]]
         model$functions[[i]]$strLaw<-gsub(" ","",toString(e[1]))
  #       r<-getRuleLeaves(mathml)
         r<-model$functions[[i]]$inputs
-        model$functions[[i]]$law=makeLaw(r,NULL,model$rules[[i]]$exprLaw) 
+        model$functions[[i]]$law=makeLaw(r,NULL,model$functions[[i]]$exprLaw) 
       }
     }
     
