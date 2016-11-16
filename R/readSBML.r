16cac2f7afbb009ac5e02b426e529f46305a1cfa
hazbib <hamzeh.zbib@pnnl.gov>
2016-11-16 12:47:31 -0800

    i

--- R/readSBML.r
+++ R/readSBML.r
@@ -535,7 +535,7 @@
         mathml=functions[[i]][["math"]][["lambda"]][["bvar"]][[1]]
 #        mathml<-model$functions[[i]]$mathmlLaw
         e=mathml2R(mathml)
-        model$functions[[i]]$exprLaw=e[[1]]
+        model$functions[[i]]$exprLaw=e[[i]]
         model$functions[[i]]$strLaw=gsub(" ","",toString(e[1]))
         r<-model$functions[[i]]$inputs
         model$functions[[i]]$law=makeLaw(r,NULL,model$functions[[i]]$exprLaw) 
